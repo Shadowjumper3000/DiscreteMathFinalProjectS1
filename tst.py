18bb3fc7909e47c6d059850ee64038d4e983daf2
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2023-11-30 22:41:18 +0100

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,5 +1,12 @@
-# idfk
-i = 5736
-PrivateKeyD = 1181
-PrivateKeyN = 6059
-print(str(pow(int(i), PrivateKeyD, PrivateKeyN)))
+message = input("Enter encoded: ")  # Remove the str() conversion
+
+encoded_list = message.strip("[]").split(",")  # Split the input list by comma and remove square brackets
+encoded_string = ""
+
+for code in encoded_list:
+    code = code.strip()  # Remove any leading or trailing whitespace
+    code = code.zfill(4)  # Fill each number to 4 characters with leading zeroes
+    encoded_string += code
+
+print("-------------")
+print(encoded_string)
